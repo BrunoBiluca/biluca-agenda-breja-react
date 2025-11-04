fa933dedfa71930034f4521d138ad2602ea74d80
BrunoBiluca <b.b.da.costa@gmail.com>
2025-11-04 19:04:09 -0300

    ✨ Início do desenvolvimento da página de cervejarias

---
+++ src/breweries/Breweries.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Breweries } from "./Breweries";
+
+test("should have the main components", () => {
+//   render(<Breweries />);
+
+//   // Query by text content
+//   const element = screen.getByText("Hello Vitest!");
+//   expect(element).toBeInTheDocument();
+});
